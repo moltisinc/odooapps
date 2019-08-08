eb4960623a34cfc390ee5b7747268f175a43fd58
Jaimik Panchal <jaimik@deneroteam.com>
2019-08-08 19:45:16 +0800

    [ADD]: Add Menu tooltip for odoo.

---
+++ menu_tooltip/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Showing a Menu ToolTip in Odoo",
+    "summary": """
+        Showing a Menu ToolTip in Odoo
+        """,
+    "description": """
+        Showing a Menu ToolTip in Odoo 
+    """,
+    "author": "Moltis Technologies",
+    "website": "www.moltis.net",
+    "support": "info@moltis.net",
+    "category": "Tools",
+    "version": "11.0.0.1",
+    "depends": ["base", "web"],
+    "data": [
+        "views/template.xml",
+        "views/action_view.xml",
+    ],
+    "license": "GPL-3",
+    "application": True,
+    "installable": True,
+}
