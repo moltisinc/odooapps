b794bdfd6ebad314fce0acc3b7d5683347250c91
Jaimik Panchal <jaimik@deneroteam.com>
2019-09-19 14:32:24 +0800

    [ADD]: user limits

---
+++ user_limit_config/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Users Limits",
+    'summary': """
+        This Module set user creation limit.""",
+    'description': """
+        This Module set user creation limit.
+    """,
+    "author": "Moltis Technologies",
+    "website": "www.moltis.net",
+    "support": "info@moltis.net",
+    'category': 'Tools',
+    "version": "12.0.0.1",
+    'depends': ['base'],
+    'data': [],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+    'license': 'AGPL-3',
+}
