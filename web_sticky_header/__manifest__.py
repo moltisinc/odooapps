671f4b8831ac8c738bb8f422d0ab9985151da372
Jaimik Panchal <jaimik@deneroteam.com>
2019-11-13 15:46:04 +0800

    [ADD]: add Web form sticky header

---
+++ web_sticky_header/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Web Form Sticky Header",
+    "version": "12.0.0.1",
+    "author": "Moltis Technologies",
+    "website": "www.moltis.net",
+    "support": "info@moltis.net",
+    "images": ["static/description/banner.png"],
+    "license": "AGPL-3",
+    "category": "Tools",
+    "summary": "Web Form Sticky Header",
+    "description": """
+        Web Form Sticky Header.
+    """,
+    "depends": [
+        "web"
+    ],
+    "data": [
+        "views/templates.xml",
+    ],
+    "demo": [],
+    "qweb": [],
+    "license": "GPL-3",
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+}
